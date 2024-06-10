d0746b025cf59edbf7d5bf213860453a10c6c2e6
Tien Huy <tienhuy.tech@gmail.com>
2024-06-10 09:18:59 +0700

    Commit Project

--- app/src/main/java/com/example/foodreciple/AdminActivity/Admin_Main.java
+++ app/src/main/java/com/example/foodreciple/AdminActivity/Admin_Main.java
@@ -1,10 +1,12 @@
 package com.example.foodreciple.AdminActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.foodreciple.Activity.DangNhap;
 import com.example.foodreciple.Activity.Intro;
+import com.example.foodreciple.Activity.MainActivity;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,22 +25,32 @@ import com.example.foodreciple.databinding.ActivityAdminMainBinding;
 import com.example.foodreciple.R;
 
 public class Admin_Main extends AppCompatActivity {
-    Button btnManageFoodDetails;
+    Button btnManageFoodDetails, Home;
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityAdminMainBinding binding;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
 
         btnManageFoodDetails = findViewById(R.id.btnManageFoodDetails);
+        Home = findViewById(R.id.Home);
 
         btnManageFoodDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Admin_Main.this, Admin_Food.class);
+                startActivity(intent);
+            }
+        });
+
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Admin_Main.this, MainActivity.class);
                 startActivity(intent);
             }
         });
